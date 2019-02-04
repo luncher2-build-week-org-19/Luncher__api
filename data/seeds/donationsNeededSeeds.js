8606d47d7e934d5e7eb57a5e791841e5470b34bd
Civoremo <omerovic_n@hotmail.com>
2019-02-04 15:08:21 -0500

    donationsNeeded migration table and seed

---
+++ data/seeds/donationsNeededSeeds.js
@@ -0,0 +1,41 @@
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex("donationsNeeded")
+        .truncate()
+        .then(function() {
+            // Inserts seed entries
+            return knex("donationsNeeded").insert([
+                {
+                    title: "No Kid Starving",
+                    description:
+                        "Give the gift of food to children and help them become all that they can become!",
+                    amount: 5000,
+                    userId: 1,
+                    schoolId: 6,
+                },
+                {
+                    title: "Food for Thought",
+                    description: "Why isn't a group of squid called a squad?",
+                    amount: 3000,
+                    userId: 1,
+                    schoolId: 1,
+                },
+                {
+                    title: "Healthy Breakfasts",
+                    description:
+                        "Help provide the necessary caloric intake of some of our youngest students.",
+                    amount: 2500,
+                    userId: 2,
+                    schoolId: 2,
+                },
+                {
+                    title: "State Spelling Bee Champs",
+                    description:
+                        "Help make the celebration of this great achievment a memorable one.",
+                    amount: 900,
+                    userId: 1,
+                    schoolId: 2,
+                },
+            ]);
+        });
+};
