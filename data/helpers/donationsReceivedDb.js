f7d4effb922bc7008bdcce30fb830adb358dbce2
Civoremo <omerovic_n@hotmail.com>
2019-02-06 00:06:37 -0500

    donationsReceived getById endpoint

--- data/helpers/donationsReceivedDb.js
+++ data/helpers/donationsReceivedDb.js
@@ -2,7 +2,33 @@ const db = require("../dbConfig.js");
 
 module.exports = {
     getAllDonated,
+    getDonatedById,
 };
+
+function getDonatedById(id) {
+    const donated = db("donationsReceived").where({ donationId: id });
+    const users = db("users").select("id", "username");
+
+    return Promise.all([donated, users]).then(res => {
+        let total = 0;
+
+        for (let i = 0; i < res[0].length; i++) {
+            total += res[0][i].amount;
+            for (let z = 0; z < res[1].length; z++) {
+                if (res[0][i].userId === res[1][z].id) {
+                    res[0][i] = {
+                        ...res[0][i],
+                        username: res[1][z].username,
+                    };
+                }
+            }
+        }
+
+        [res] = res;
+
+        return { res, totalRecived: total };
+    });
+}
 
 function getAllDonated() {
     const donated = db("donationsReceived");
--- data/routes/donations/donationsReceivedRouter.js
+++ data/routes/donations/donationsReceivedRouter.js
@@ -4,6 +4,19 @@ const { protected, checkRole } = require("../../middleware/protectedMW.js");
 const donatedDb = require("../../helpers/donationsReceivedDb.js");
 const router = express.Router();
 
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+
+    donatedDb
+        .getDonatedById(id)
+        .then(donated => {
+            res.status(200).json(donated);
+        })
+        .catch(err => {
+            res.status(500).json({ err });
+        });
+});
+
 router.get("/all", (req, res) => {
     donatedDb
         .getAllDonated()
