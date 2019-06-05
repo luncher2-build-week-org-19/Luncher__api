56533f128cd6ac3503a54ce7385d45b91061b6e3
Civoremo <omerovic_n@hotmail.com>
2019-06-05 15:08:18 -0400

    postgres development

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,10 @@
 // Update with your config settings.
+require('dotenv').config();
+
+const pg = require('pg');
+pg.defaults.ssl = false;
+
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
     development: {
@@ -32,18 +38,14 @@ module.exports = {
     },
 
     production: {
-        client: "postgresql",
-        connection: {
-            database: "my_db",
-            user: "username",
-            password: "password",
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
+        client: "PGl",
+        connection: dbConnection,
+        useNullAsDefault: true,
         migrations: {
-            tableName: "knex_migrations",
+            directory: './data/migrations'
         },
+        seeds: {
+            directory: './data/seeds'
+        }
     },
 };
