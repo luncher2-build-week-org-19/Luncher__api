ee3646f1953a870509fa39bdb1534b0d92fd26d7
Civoremo <omerovic_n@hotmail.com>
2019-06-05 18:02:06 -0400

    schools seed file fix

--- data/seeds/02_schools.js
+++ data/seeds/02_schools.js
@@ -10,7 +10,7 @@ exports.seed = function(knex, Promise) {
                   image:
                       "https://cdn.pixabay.com/photo/2014/04/03/00/43/lion-309219_960_720.png",
                   schoolname: "Boone High School",
-                  userId: 2,
+                  userId: 0,
               },
               { image: "", schoolname: "Winter Park High School", userId: 2 },
               {
@@ -23,19 +23,19 @@ exports.seed = function(knex, Promise) {
                   image:
                       "https://i.pinimg.com/originals/ca/2f/19/ca2f19b5c0a37481a3a731a629fc2346.png",
                   schoolname: "Pine Hills Elementary School",
-                  userId: 1,
+                  userId: 3,
               },
               {
                   image: "https://ctftime.org/media/team/raccoon3.png",
                   schoolname: "Meadow Creek Elementary School",
                   userId: 1,
               },
-              {
-                  image:
-                      "https://www.clipartmax.com/png/middle/222-2220631_dragon-clipart-black-and-white-school-clipart-dragon-public-domain.png",
-                  schoolname: "Southchase Elementary School",
-                  userId: 2,
-              },
+              // {
+              //     image:
+              //         "https://www.clipartmax.com/png/middle/222-2220631_dragon-clipart-black-and-white-school-clipart-dragon-public-domain.png",
+              //     schoolname: "Southchase Elementary School",
+              //     userId: 2,
+              // },
           ]);
       });
 };
