2cc116da217af272282e3187fa76c52f4b6be5dc
Civoremo <omerovic_n@hotmail.com>
2019-06-05 18:35:43 -0400

    another fix

--- data/seeds/02_schools.js
+++ data/seeds/02_schools.js
@@ -16,26 +16,26 @@ exports.seed = function(knex, Promise) {
               {
                   image: "",
                   schoolname: "Washington Middle School",
-                  userId: 2,
-              },
-              { image: "", schoolname: "Walker Middle School", userId: 1 },
-              {
-                  image:
-                      "https://i.pinimg.com/originals/ca/2f/19/ca2f19b5c0a37481a3a731a629fc2346.png",
-                  schoolname: "Pine Hills Elementary School",
                   userId: 3,
               },
-              {
-                  image: "https://ctftime.org/media/team/raccoon3.png",
-                  schoolname: "Meadow Creek Elementary School",
-                  userId: 1,
-              },
-              {
-                  image:
-                      "https://www.clipartmax.com/png/middle/222-2220631_dragon-clipart-black-and-white-school-clipart-dragon-public-domain.png",
-                  schoolname: "Southchase Elementary School",
-                  userId: 2,
-              },
+              // { image: "", schoolname: "Walker Middle School", userId: 1 },
+              // {
+              //     image:
+              //         "https://i.pinimg.com/originals/ca/2f/19/ca2f19b5c0a37481a3a731a629fc2346.png",
+              //     schoolname: "Pine Hills Elementary School",
+              //     userId: 3,
+              // },
+              // {
+              //     image: "https://ctftime.org/media/team/raccoon3.png",
+              //     schoolname: "Meadow Creek Elementary School",
+              //     userId: 1,
+              // },
+              // {
+              //     image:
+              //         "https://www.clipartmax.com/png/middle/222-2220631_dragon-clipart-black-and-white-school-clipart-dragon-public-domain.png",
+              //     schoolname: "Southchase Elementary School",
+              //     userId: 2,
+              // },
           ]);
       });
 };
