21317c10a5c6a1525fda796e1d90f22f309f3e7e
Civoremo <omerovic_n@hotmail.com>
2019-06-05 18:50:28 -0400

    lets try another fix

--- data/seeds/02_schools.js
+++ data/seeds/02_schools.js
@@ -5,38 +5,37 @@ exports.seed = function(knex, Promise) {
       .then(function() {
           // Inserts seed entries
           return knex("schools").insert([
-              { id: 0, image: "", schoolname: "Oak Ridge High School", userId: 1 },
+              { image: "", schoolname: "Oak Ridge High School", userId: 1 },
               // {
               //     image:
               //         "https://cdn.pixabay.com/photo/2014/04/03/00/43/lion-309219_960_720.png",
               //     schoolname: "Boone High School",
               //     userId: 0,
               // },
-              { id: 1, image: "", schoolname: "Winter Park High School", userId: 2 },
+              { image: "", schoolname: "Winter Park High School", userId: 2 },
               {
-                  id: 2,
                   image: "",
                   schoolname: "Washington Middle School",
                   userId: 3,
               },
-              // { image: "", schoolname: "Walker Middle School", userId: 1 },
-              // {
-              //     image:
-              //         "https://i.pinimg.com/originals/ca/2f/19/ca2f19b5c0a37481a3a731a629fc2346.png",
-              //     schoolname: "Pine Hills Elementary School",
-              //     userId: 3,
-              // },
-              // {
-              //     image: "https://ctftime.org/media/team/raccoon3.png",
-              //     schoolname: "Meadow Creek Elementary School",
-              //     userId: 1,
-              // },
-              // {
-              //     image:
-              //         "https://www.clipartmax.com/png/middle/222-2220631_dragon-clipart-black-and-white-school-clipart-dragon-public-domain.png",
-              //     schoolname: "Southchase Elementary School",
-              //     userId: 2,
-              // },
+              { image: "", schoolname: "Walker Middle School", userId: 1 },
+              {
+                  image:
+                      "https://i.pinimg.com/originals/ca/2f/19/ca2f19b5c0a37481a3a731a629fc2346.png",
+                  schoolname: "Pine Hills Elementary School",
+                  userId: 3,
+              },
+              {
+                  image: "https://ctftime.org/media/team/raccoon3.png",
+                  schoolname: "Meadow Creek Elementary School",
+                  userId: 1,
+              },
+              {
+                  image:
+                      "https://www.clipartmax.com/png/middle/222-2220631_dragon-clipart-black-and-white-school-clipart-dragon-public-domain.png",
+                  schoolname: "Southchase Elementary School",
+                  userId: 2,
+              },
           ]);
       });
 };
